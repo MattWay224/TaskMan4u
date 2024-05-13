cd4c47acdc55270788709e2588d743869a9d3082
zDragonLORD <44378815+zDragonLORD@users.noreply.github.com>
2024-05-13 19:27:35 +0300

    Add files via upload

--- database.sql
+++ database.sql
@@ -1,17 +1,15 @@
-
 CREATE TABLE IF NOT EXISTS Users (
-    id INTEGER AUTOINCREMENT,
-    user_id INTEGER PRIMARY KEY,
-    name VARCHAR(50),
-    password VARCHAR(50),
-    has_admin_rights BOOLEAN
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(50) UNIQUE,
+    password VARCHAR(50)
 );
 
 CREATE TABLE IF NOT EXISTS Todos (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id INTEGER,
     todo_text TEXT,
-    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+    todo_date DATE,
+    FOREIGN KEY(user_id) REFERENCES Users(id)
 );
 
-INSERT INTO Users (name, password, has_admin_rights) VALUES ('admin', 'admin_pass', TRUE);
+INSERT INTO Users (id, name, password) VALUES (1, 'admin', 'password');
